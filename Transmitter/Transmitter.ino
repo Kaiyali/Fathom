e251a6973b503c481ec5e9a9f360d0d6bfa22eaa
Kaiyalii <80181145+Kaiyali@users.noreply.github.com>
2024-01-16 17:57:21 -0500

    Update Transmitter.ino
    The final functional tested code for the transmitter

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -6,6 +6,9 @@
 #define VRX_PIN  A0 // Arduino pin connected to VRX pin
 #define VRY_PIN  A5 // Arduino pin connected to VRX pin
 
+
+#define BTN_BTM_LEFT  0
+
 RF24 radio(9, 8); // CE, CSN
 const byte address[10] = "FATHOMADD";
 
@@ -15,6 +18,9 @@ const char txtLeft[] = "LEFT";
 const char txtRight[] = "RIGHT";
 const char txtBack[] = "BWD";
 const char txtFront[] = "FWD";
+const char ytxtCenter[] = "CTNY";
+const char xtxtCenter[] = "CTNX";
+const char txtKill[] = "KILL";
 
 void setup()
 {
@@ -22,6 +28,7 @@ void setup()
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
+  pinMode(BTN_BTM_LEFT, INPUT_PULLUP);
 }
 
 void loop()
@@ -32,23 +39,28 @@ void loop()
   int xValue = map(rawXValue, 0, 1023, -100, 100); // Maps the raw analog input value to a range of -100 to 100
   int yValue = map(rawYValue, 0, 1023, -100, 100); // Maps the raw analog input value to a range of -100 to 100
 
-  if(xValue >= 20){
-    Serial.println("it is turning left");
-    delay(1000);
+
+  byte Status_BTN_BTM_LEFT = digitalRead(BTN_BTM_LEFT);
+  if(xValue >= 10){
+    radio.write(&txtLeft, sizeof(txtLeft));
   }
-  if(xValue <= -20){
-    Serial.println("it is turning right");
-    delay(1000);
+  if(xValue <= -10){
+    radio.write(&txtRight, sizeof(txtRight));
+  }
+  if(yValue >= 10){
+    radio.write(&txtBack, sizeof(txtBack));
+  }
+  if(yValue <= -10 ){
+    radio.write(&txtFront, sizeof(txtFront));
+  } 
+  if(yValue >= -10 && yValue <= 10){
+    radio.write(&ytxtCenter, sizeof(ytxtCenter));
+  }
+  if(xValue >= -10 && xValue <= 10){
+    radio.write(&xtxtCenter, sizeof(xtxtCenter));
+  }
+  if (Status_BTN_BTM_LEFT == LOW) {
+    radio.write(&txtKill, sizeof(txtKill));
   }
   
-  if(yValue >= 20){
-    Serial.println("it is turning backward");
-    delay(1000);
-  }
-  if(yValue <= -20 ){
-    Serial.println("it is turning forward");
-    delay(1000);
-  } 
-
-  delay(1000);
 }
